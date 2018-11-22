9830a1f17dae097348b9deebc114a4af4ee308ce
Steven Kay <stevendkay@gmail.com>
2018-11-22 15:12:50 +0000

    got plan of attack

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,6 +1,19 @@
 const handleSubmit = (event) => {
   event.preventDefault();
   console.log("Hello world!");
+  const title = this.title.value;
+  const author = this.author.value;
+  const category = this.category.value;
+  const injection = document.querySelector('#reading-list');
+
+  // make book div
+
+  // make 3 child divs (title, author, category)
+
+  // attach children to book divs
+
+  // attach book div to injection point
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
