a1b01f42fe6806d29067527381888d1df6efe3c9
Steven Kay <stevendkay@gmail.com>
2018-11-22 15:23:17 +0000

    time for css

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -9,10 +9,22 @@ const handleSubmit = (event) => {
   // make book div
   const newBook = document.createElement("div");
   newBook.classList.add("book");
-  
+
   // make 3 child divs (title, author, category)
+  const newTitle = document.createElement("div");
+  newTitle.classList.add("title");
+  newTitle.textContent = title;
+  const newAuthor = document.createElement("div");
+  newAuthor.classList.add("author");
+  newAuthor.textContent = author;
+  const newCategory = document.createElement("div");
+  newCategory.classList.add("category");
+  newCategory.textContent = category;
 
   // attach children to book divs
+  newBook.appendChild(newTitle);
+  newBook.appendChild(newAuthor);
+  newBook.appendChild(newCategory);
 
   // attach book div to injection point
   injection.appendChild(newBook);
